6d8f6d0636c999e9210e09356af0278d12c69f71
Racheljjn <jian1160@mylaurier.ca>
2020-09-19 13:50:23 -0400

    add stylings to menu-items

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
-import './App.css';
-import Home from './home'
-import './home.styles.css'
+import Home from './pages/home/home'
+
 
 function App() {
   return (
---
+++ src/pages/home/home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Menu from '../../components/menu/menu.jsx'
+import '../home/home.styles.scss'
+
+const Home = () => {
+ return (<div className="home">
+  <Menu />
+ </div>)
+}
+
+export default Home;
