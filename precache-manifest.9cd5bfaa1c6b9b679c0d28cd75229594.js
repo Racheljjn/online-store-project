b7b2473d5853f59c82f336ad29b8fbe9d6d6bf94
Racheljjn <jian1160@mylaurier.ca>
2020-09-22 11:57:09 -0400

    Updates

---
+++ precache-manifest.9cd5bfaa1c6b9b679c0d28cd75229594.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4324497a81ed62b64d8ec5fa0032c83d",
+    "url": "/online-store-project/index.html"
+  },
+  {
+    "revision": "2e78d3f19d0eae8eb9e9",
+    "url": "/online-store-project/static/css/main.7f400213.chunk.css"
+  },
+  {
+    "revision": "11cddb36dda1758bd55c",
+    "url": "/online-store-project/static/js/2.885378ef.chunk.js"
+  },
+  {
+    "revision": "d0b13a04dd0dc4c851ad16fad1cc6f51",
+    "url": "/online-store-project/static/js/2.885378ef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2e78d3f19d0eae8eb9e9",
+    "url": "/online-store-project/static/js/main.dd2d88b6.chunk.js"
+  },
+  {
+    "revision": "b7592f7436700e7fba71",
+    "url": "/online-store-project/static/js/runtime-main.1c911300.js"
+  },
+  {
+    "revision": "ed28accb7dd58a0469bff983503ff07e",
+    "url": "/online-store-project/static/media/crown.ed28accb.svg"
+  }
+]);
