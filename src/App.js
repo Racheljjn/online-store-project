57d31b82df686896a3927ac5425a0102210dc480
Racheljjn <jian1160@mylaurier.ca>
2020-09-22 13:36:25 -0400

    fix bugs

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import Home from './pages/home/home'
-import { Switch, Route } from 'react-router-dom'
+import { Switch, Route, HashRouter, Link } from 'react-router-dom'
 import Shop from './pages/shop/Shop'
 import Header from './components/header/header'
 import SignInAndSignUpPage from './pages/sign-in and sign-up/sign-in-sign-up'
 
 function App () {
   return (
+    <HashRouter basename='/'>
     <div>
       <Header />
       <Switch>
@@ -15,6 +16,7 @@ function App () {
         <Route path='/signin' component={SignInAndSignUpPage} />
       </Switch>
     </div>
+    </HashRouter>
   )
 }
 
